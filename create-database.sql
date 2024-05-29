26501cb9335d00e464f898cf90b2c07d1a1a4fc2
Dariusz Krysmann <dkrysmann@anwb.nl>
2024-05-29 14:33:21 +0200

    delete example db

--- create-database.sql
+++ create-database.sql
@@ -1,16 +1,3 @@
-CREATE DATABASE IF NOT EXISTS example_db;
-USE DATABASE example_db;
-CREATE SCHEMA IF NOT EXISTS example_schema;
-USE SCHEMA example_schema;
-
-CREATE OR REPLACE TABLE employees(id NUMBER, name VARCHAR, role VARCHAR);
-INSERT INTO employees (id, name, role) VALUES 
-(1, 'Rene', 'op'), 
-(2, 'Ewin', 'dev'), 
-(3, 'Frank', 'dev'),
-(4, 'Darek', 'op'), 
-(5, 'Jean-Paul', 'op'), 
-(6, 'Maurice', 'dev'), 
-(7, 'Peter', 'dev'),
-(8, 'Davina', 'op')
-;
+DROP TABLE IF EXISTS employees;
+DROP SCHEMA example_schema;
+DROP DATABASE example_db;
