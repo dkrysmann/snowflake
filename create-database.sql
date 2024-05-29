fd50ac2f4f95f8927e0aac50f7e81c7306aa1c52
Dariusz Krysmann <dkrysmann@anwb.nl>
2024-05-29 14:23:36 +0200

    update table employees

--- create-database.sql
+++ create-database.sql
@@ -1,7 +1,15 @@
-CREATE DATABASE example_db;
+employeesCREATE DATABASE example_db;
 USE DATABASE example_db;
 CREATE SCHEMA example_schema;
 USE SCHEMA example_schema;
 
 CREATE OR REPLACE TABLE employees(id NUMBER, name VARCHAR, role VARCHAR);
-INSERT INTO employees (id, name, role) VALUES (1, 'Alice', 'op'), (2, 'Bob', 'dev'), (3, 'Cindy', 'dev');
+INSERT INTO employees (id, name, role) VALUES 
+(1, 'Rene', 'op'), 
+(2, 'Ewin', 'dev'), 
+(3, 'Frank', 'dev'),
+(4, 'Darek', 'op'), 
+(5, 'Jean-Paul', 'op'), 
+(6, 'Maurice', 'dev'), 
+(7, 'Peter', 'dev')
+;
