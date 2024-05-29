a2836e2ded3d70f6a71a05cbc03eea2c04c1c9d3
Dariusz Krysmann <dkrysmann@anwb.nl>
2024-05-29 14:17:15 +0200

    update create database

--- create-database.sql
+++ create-database.sql
@@ -1,9 +1,7 @@
-CREATE OR REPLACE TABLE  customers_git AS
-SELECT 
-    ABS(RANDOM()) AS CUSTOMER_ID,
-    FAKE('en_US','first_name',null)::varchar AS FIRST_NAME,
-    FAKE('en_US','last_name',null)::varchar AS LAST_NAME,
-    FAKE('en_US','phone_number',null)::varchar AS PHONE_NO,
-    FAKE('en_US','free_email',null)::varchar AS EMAIL,
-    FAKE('en_US','state_abbr',null)::varchar AS STATE
-FROM TABLE(GENERATOR(ROWCOUNT => 1000));
+CREATE DATABASE example_db;
+USE DATABASE example_db;
+CREATE SCHEMA example_schema;
+USE SCHEMA example_schema;
+
+CREATE OR REPLACE TABLE employees(id NUMBER, name VARCHAR, role VARCHAR);
+INSERT INTO employees (id, name, role) VALUES (1, 'Alice', 'op'), (2, 'Bob', 'dev'), (3, 'Cindy', 'dev');
